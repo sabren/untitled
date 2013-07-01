81bf4a0af0f1c71bad230b5329969500ebd9ee40
sabren <michal.wallace@gmail.com>
2013-07-01 02:08:59 -0500

    added a tiny sortable datasheet

---
+++ static/datasheet.js
@@ -0,0 +1,18 @@
+YUI().use("datatable", function (Y) {
+
+  // A table from data with keys that work fine as column names
+  var simple = new Y.DataTable({
+    columns: ["id", "name", "price"],
+    data   : [
+      { id: "ga_3475", name: "gadget",   price: "$6.99" },
+      { id: "sp_9980", name: "sprocket", price: "$3.75" },
+      { id: "wi_0650", name: "widget",   price: "$4.25" }
+    ],
+    summary: "Price sheet for inventory parts",
+    caption: "Example table with simple columns",
+    sortable: ["id", "name", "price"]
+  });
+
+  simple.render("#simple");
+
+});
